316930abd09a4b398fd5a8c61a2f9ad710f3978e
Kelly Neil Ang <angkellyn14@gmail.com>
2026-04-21 01:19:46 +0800

    Add sidebar features: logo, sign out, active tab styling

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,47 +1,75 @@
-import { FaHome, FaUsers, FaBuilding, FaMoneyBill, FaInfoCircle } from "react-icons/fa";
+import { FaHome, FaUsers, FaBuilding, FaMoneyBill, FaInfoCircle, FaSignOutAlt } from "react-icons/fa";
 import { useContext } from "react";
 import styles from "./Sidebar.module.css";
 import { AppContext } from "../App";
 
 export default function Sidebar() {
-    const { setCurrentPage } = useContext(AppContext);
+    const { session, setCurrentPage } = useContext(AppContext);
 
     return (
         <>
             <div className={styles["sidebar"]}>
                 <div className={styles["sidebarPanel"]}>
-                    <span style={{ fontSize: "24px", color: "var(--accent)", fontWeight: "bold" }}> App Name Here </span>
-                    <SidebarTab onClick={() => setCurrentPage({ name: 'dashboard' })} icon={FaHome}> Dashboard </SidebarTab>
+                    <img width={180} src="/logo.png"></img>
 
-<SidebarTab onClick={() => setCurrentPage({ name: 'clients' })} icon={FaUsers}> Clients </SidebarTab>
+                    <SidebarTab activePage="dashboard" onClick={() => setCurrentPage({ name: 'dashboard' })} icon={FaHome}> Dashboard </SidebarTab>
 
-<SidebarTab onClick={() => setCurrentPage({ name: 'properties' })} icon={FaBuilding}> Properties </SidebarTab>
+                    <SidebarTab activePage="client" onClick={() => setCurrentPage({ name: 'clients' })} icon={FaUsers}> Clients </SidebarTab>
 
-<SidebarTab onClick={() => setCurrentPage({ name: 'payments' })} icon={FaMoneyBill}> Payments </SidebarTab>
+                    <SidebarTab activePage="propert" onClick={() => setCurrentPage({ name: 'properties' })} icon={FaBuilding}> Properties </SidebarTab>
 
-<SidebarTab onClick={() => setCurrentPage({ name: 'about' })} icon={FaInfoCircle}> About </SidebarTab>
+                    <SidebarTab activePage="payment" onClick={() => setCurrentPage({ name: 'payments' })} icon={FaMoneyBill}> Payments </SidebarTab>
+
+                    <SidebarTab activePage="about" onClick={() => setCurrentPage({ name: 'about' })} icon={FaInfoCircle}> About </SidebarTab>
                     
+                </div>
+                <div className={styles["bottomPanel"]}>
+                    <span> Hello, {session.current.username} ({session.current.email}) </span>
+                    <button className="flex items-center gap-2" onClick={() => session.signOut()}>
+                        <FaSignOutAlt/> Log out
+                    </button>
                 </div>
             </div>
         </>
     );
 }
 
-function TemporaryIconPlaceholder() {
-    return (
-        <div style={{ display: "inline", minHeight: "48px", minWidth: "48px", backgroundColor: "green" }}>
-            icon
-        </div>
-    )
-}
-
-function SidebarTab({ onClick, icon, children }) {
+function SidebarTab({ onClick, icon, children, activePage }) {
     const IconComponent = icon;
 
-    return (
+    const { currentPage } = useContext(AppContext);
+
+    const isActive = (page) => page.toLowerCase().includes(activePage); 
+
+    const iconStyle = (page) => {
+        if (isActive(page)) {
+            return {
+                color: "var(--accent)",
+                fontSize: "24px"
+            }
+        } else {
+            return {
+                fontSize: "24px"
+            }
+        }
+    };
+
+    const textStyle = (page) => {
+        if (isActive(page)) {
+            return {
+                fontWeight: "bold"
+            }
+        } else {
+            return {
+                color: "gray"
+            }
+        }
+    }
+
+    return (    
         <div className={styles["sidebarTab"]} onClick={onClick}>
-            {IconComponent ? <IconComponent /> : null}
-            {children}
+            {IconComponent ? <IconComponent style={iconStyle(currentPage.name)} /> : null}
+            <span style={textStyle(currentPage.name)}> {children} </span>
         </div>
     )
 }
